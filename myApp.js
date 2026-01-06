c9f68b1a8cc9de2e797f4d329a7b3448b463dfe4
reiidj <117252879+reiidj@users.noreply.github.com>
2026-01-06 23:44:19 +0800

    Removed extra app.listen

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+const PORT = process.env.PORT || 3030;
 
 
 
@@ -57,6 +58,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+app.listen(PORT, ()=> {
+  console.log(`Server is running on port ${PORT}`);
 });
